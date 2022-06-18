660bfcd60461347f7cb119b70a9befe08d1202cc
= <=>
2022-06-18 10:12:59 +0700

    login and signup Doctor Patient

--- HospitalManagement/Hospital/urls.py
+++ HospitalManagement/Hospital/urls.py
@@ -1,8 +1,16 @@
 from django import views
 from django.contrib import admin
 from django.urls import path,include
-from .views import home_view
+from .views import home_view,doctor_signup_view,patient_signup_view,afterlogin_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('',home_view,name=""),
+    path('login', LoginView.as_view(template_name='hospital/login.html'),name='login'),
+    
+    path('doctorsignup', doctor_signup_view, name='doctorsignup'),
+    path('patientsignup', patient_signup_view),
+
+    path('afterlogin', afterlogin_view,name='afterlogin'),
+    path('logout', LogoutView.as_view(template_name='hospital/index.html'),name='logout'),
 ]
--- HospitalManagement/Hospital/views.py
+++ HospitalManagement/Hospital/views.py
@@ -1,7 +1,83 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required,user_passes_test
+from . import forms, models
+from django.contrib.auth.models import Group
 
 # Create your views here.
 
 def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
     return render(request,'hospital/index.html')
+
+# def login_view(request):
+#     return render(request,'hospital/login.html')
+
+def doctor_signup_view(request):
+    userForm=forms.DoctorUserForm()
+    doctorForm=forms.DoctorForm()
+    mydict={'userForm':userForm,'doctorForm':doctorForm}
+    if request.method=='POST':
+        print("ok")
+        userForm=forms.DoctorUserForm(request.POST)
+        doctorForm=forms.DoctorForm(request.POST,request.FILES)
+        if userForm.is_valid() and doctorForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            doctor=doctorForm.save(commit=False)
+            doctor.user=user
+            doctor=doctor.save()
+            my_doctor_group = Group.objects.get_or_create(name='DOCTOR')
+            my_doctor_group[0].user_set.add(user)
+        return HttpResponseRedirect('login')
+    return render(request,'hospital/doctor_signup.html',context=mydict)
+
+def patient_signup_view(request):
+    userForm=forms.PatientUserForm()
+    patientForm=forms.PatientForm()
+    mydict={'userForm':userForm,'patientForm':patientForm}
+    if request.method=='POST':
+        userForm=forms.PatientUserForm(request.POST)
+        patientForm=forms.PatientForm(request.POST,request.FILES)
+        if userForm.is_valid() and patientForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            patient=patientForm.save(commit=False)
+            patient.user=user
+            patient.assignedDoctorId=request.POST.get('assignedDoctorId')
+            patient=patient.save()
+            my_patient_group = Group.objects.get_or_create(name='PATIENT')
+            my_patient_group[0].user_set.add(user)
+        return HttpResponseRedirect('login')
+    return render(request,'hospital/patient_signup.html',context=mydict)
+
+def is_admin(user):
+    return user.groups.filter(name='ADMIN').exists()
+def is_doctor(user):
+    return user.groups.filter(name='DOCTOR').exists()
+def is_patient(user):
+    return user.groups.filter(name='PATIENT').exists()
+
+def afterlogin_view(request):
+    if is_admin(request.user):
+        # return redirect('admin-dashboard')
+        return HttpResponse("Admin")
+    elif is_doctor(request.user):
+        accountapproval=models.Doctor.objects.all().filter(user_id=request.user.id,status=True)
+        if accountapproval:
+            # return redirect('doctor-dashboard')
+            return HttpResponse("Doctor")
+        else:
+            # return render(request,'hospital/doctor_wait_for_approval.html')
+            return HttpResponse("Doctor cho xet")
+    elif is_patient(request.user):
+        accountapproval=models.Patient.objects.all().filter(user_id=request.user.id,status=True)
+        if accountapproval:
+            # return redirect('patient-dashboard')
+            return HttpResponse("Patient")
+        else:
+            # return render(request,'hospital/patient_wait_for_approval.html')
+            return HttpResponse("Patient Cho Xet")
